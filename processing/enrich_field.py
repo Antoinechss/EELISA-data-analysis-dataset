4c8e2f1bdd9b01567f1e38f72b3ea4cd558d47fd
Antoinechss <antoine.chosson@eleves.enpc.fr>
2025-12-02 21:16:06 +0100

    finished dataset

--- processing/enrich_field.py
+++ processing/enrich_field.py
@@ -1,26 +1,11 @@
 import pandas as pd
 from scraping.config import FIELD_KEYWORDS
 
-jobs_dataset = '/Users/antoinechosson/Desktop/EELISA/EELISA-Data-analysis/eures_jobs_full.csv'
+jobs_dataset = '/Users/antoinechosson/Desktop/EELISA/EELISA-Data-analysis/datasets/european_jobs.csv'
 df = pd.read_csv(jobs_dataset)
 
-# ------------------------
-# Infering field from job description
-# ------------------------
+### INFER FIELD FROM TITLE AND DESCRIPTION ###
 
-
-def infer_field(description):
-    desc = str(description).lower()
-    for field, keywords in FIELD_KEYWORDS.items():
-        for kw in keywords:
-            if kw in desc:
-                return field
-    return "Other / Undefined"
-
-
-mask = df['field'] == "Other / Undefined"
-df.loc[mask, 'field'] = df.loc[mask, 'full_description'].apply(infer_field)
-
-df.to_csv('/Users/antoinechosson/Desktop/EELISA/EELISA-Data-analysis/eures_jobs_full.csv', index=False)
+df.to_csv('/Users/antoinechosson/Desktop/EELISA/EELISA-Data-analysis/datasets/european_jobs_enriched_fields.csv', index=False)
 
 
